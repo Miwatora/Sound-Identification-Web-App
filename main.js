aaa97a2088d9c5edbe051ba47716713353d4b214
Miwatora <114199218+Miwatora@users.noreply.github.com>
2023-06-08 13:57:42 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
+  classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kHvp0MfAd/model.json', modelReady);
+}
+
+function modelReady(){
+  classifier.classify(gotResults);
+}
+var dog = 0;
+var cat = 0;
+
+function gotResults(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+
+    document.getElementById("result_label").innerHTML = 'Detected voice is of  - '+ results[0].label;
+    document.getElementById("result_count").innerHTML = 'Detected Dog - '+dog+ ' Detected Cat - '+cat;
+  
+
+    img = document.getElementById('animal_image');
+
+    if (results[0].label == "Barking") {
+      img.src = 'dog.gif';
+      dog = dog+1;
+    } else if (results[0].label == "Meowing") {
+      img.src = 'cat.gif';
+      cat = cat + 1;
+    } else{
+      img.src = 'listen.gif';
+    }
+  }
+}
